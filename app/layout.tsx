5c588b3043d63fbb68aa28c641170629aa6b65ac
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-07-11 21:18:53 +0500

    register modal UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+'use client';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal';
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -18,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          {/* <Modal isOpen onClose={()=>{}} onSubmit={()=>{}} actionLabel='test' title='Hello World'  /> */}
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
